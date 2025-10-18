2aefa0b92b72743767bd9148ab9d542c6bccf90d
Amonion <amonionprime@gmail.com>
2025-10-18 12:25:27 +0100

    update authentication

--- app/(home)/(auth)/signup-successful/page.tsx
+++ app/(home)/(auth)/signup-successful/page.tsx
@@ -3,7 +3,8 @@ import { generateMetadata } from '@/lib/seo'
 
 export const metadata = generateMetadata({
   title: 'Sign Up Successful | Paragon',
-  description: 'Congratulations, your account has been created successfully.',
+  description:
+    'Congratulations, your Paragon account has been created successfully.',
   twitter: {
     card: 'summary',
     site: '@paragon',
