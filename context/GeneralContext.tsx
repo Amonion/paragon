30012c572f72fdacde6b368f858a8ac9dad74099
Amonion <amonionprime@gmail.com>
2025-10-23 20:15:34 +0100

    update signup and login

--- context/GeneralContext.tsx
+++ context/GeneralContext.tsx
@@ -2,9 +2,6 @@
 import { initializeSound } from '@/lib/sound'
 import useSocket from '@/src/useSocket'
 import { MessageStore } from '@/src/zustand/notification/Message'
-import SchoolStore from '@/src/zustand/school/School'
-import { AuthStore } from '@/src/zustand/user/AuthStore'
-import OfficeStore from '@/src/zustand/utility/Office'
 import { createContext, useEffect, useContext, ReactNode, useMemo } from 'react'
 
 const GeneralContext = createContext<{
@@ -19,10 +16,7 @@ interface GeneralProviderProps {
 
 export const GeneralProvider = ({ children }: GeneralProviderProps) => {
   const socket = useSocket()
-  const { setBaseUrl, setMessage } = MessageStore()
-  const { user } = AuthStore()
-  const { getSchoolNotifications } = SchoolStore()
-  const { officeForm } = OfficeStore()
+  const { setBaseUrl } = MessageStore()
 
   useEffect(() => {
     initializeSound()
@@ -32,16 +26,6 @@ export const GeneralProvider = ({ children }: GeneralProviderProps) => {
         : process.env.NEXT_PUBLIC_DEV_API_URL
     setBaseUrl(String(url))
   }, [])
-
-  ///////////////GET SCHOOL NOTIFICATIONS///////////////
-  useEffect(() => {
-    if (user) {
-      getSchoolNotifications(
-        `/notifications/?username=${user.username}`,
-        setMessage
-      )
-    }
-  }, [officeForm])
 
   const value = useMemo(() => ({ socket }), [socket])
 
